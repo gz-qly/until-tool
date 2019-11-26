d021dbfd5348a920247ec4c98dfda866e77d4d6f
q_xq <1418543281@qq.com>
2019-11-26 11:55:39 +0800

    upload

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-module.exports.printMsg = function () {
-  console.log("my first npm package")
+
+function isEmpty(value) {
+  return [undefined, null, '', false, true].some(function (item) { return item === value; })
+    || (Array.isArray(value) && value.length === 0)
+    || (isPlainObject(value) && (function () {
+      for (var _ in value) {
+        return false;
+      }
+      return true;
+    })());
 }
+exports.isEmpty = isEmpty;
